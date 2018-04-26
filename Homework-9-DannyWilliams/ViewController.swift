930d508af83734ef682b4310b777507fcc50edc7
Daniel Williams <danielwilliams@Daniels-MacBook-Air.local>
2018-04-26 12:14:33 -0400

    Initial Commit

---
+++ Homework-9-DannyWilliams/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Homework-9-DannyWilliams
+//
+//  Created by Daniel Williams on 4/24/18.
+//  Copyright © 2018 Daniel Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberView: UITextField!
+    
+    @IBOutlet weak var factView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
